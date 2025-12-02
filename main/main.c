1d81b930f814d6e0b18dd5f536556c33bb0802fb
Piotr Wilga <pwilga@gmail.com>
2025-12-02 22:39:50 +0100

    feat(button) add reference to supervisor button adapters and others

--- main/main.c
+++ main/main.c
@@ -1,17 +1,19 @@
 #include "freertos/FreeRTOS.h" // IWYU pragma: keep
+#include "button_adapter.h"
 #include "inet_adapter.h"
 #include "rf433_adapter.h"
 #include "supervisor.h"
-// #include "zigbee_adapter.h"  // uncomment when zigbee is ready
+// #include "zigbee_adapter.h" // uncomment when zigbee is ready
 
 void app_main(void) {
     // Initialize supervisor core
     supervisor_init();
 
     // Register adapters
+    supervisor_register_adapter(&button_adapter);
     supervisor_register_adapter(&inet_adapter);
     supervisor_register_adapter(&rf433_adapter);
-    // supervisor_register_adapter(&zigbee_adapter);  // uncomment when zigbee is ready
+    // supervisor_register_adapter(&zigbee_adapter); // uncomment when zigbee is ready
 
     supervisor_platform_init();
 
